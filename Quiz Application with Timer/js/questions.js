859c323a3f0ddaec7a84e0058c1cfe81e54df959
unknown <remyalissonb@gmail.com>
2024-05-02 07:27:33 -0300

    Init

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "No mês de maio o SEST SENAT realiza uma Mobilização Nacional, qual o tema dessa mobilização?",
+    answer: "Maio Amarelo | Segurança no trânsito",
+    options: [
+      "Maio Amarelo | Segurança no trânsito",
+      "Maio Verde | Segurança no meio ambiente",
+      "Maio Branco | Segurança na cidade",
+      "Maio Cinza | conscientização sobre carbono"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Para quem se destina os serviços do SEST SENAT?",
+    answer: "Comunidade e Transporte",
+    options: [
+      "Comunidade e Transporte",
+      "Apenas Comunidade",
+      "Apenas Transporte",
+      "Nenhum"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quantas unidades do SEST SENAT tem em Alagoas?",
+    answer: "3.",
+    options: [
+      "2.",
+      "3.",
+      "1.",
+      "5."
+    ]
+  },
+  //   {
+  //   numb: 4,
+  //   question: "What does SQL stand for?",
+  //   answer: "Structured Query Language",
+  //   options: [
+  //     "Stylish Question Language",
+  //     "Stylesheet Query Language",
+  //     "Statement Question Language",
+  //     "Structured Query Language"
+  //   ]
+  // },
+  //   {
+  //   numb: 5,
+  //   question: "What does XML stand for?",
+  //   answer: "eXtensible Markup Language",
+  //   options: [
+  //     "eXtensible Markup Language",
+  //     "eXecutable Multiple Language",
+  //     "eXTra Multi-Program Language",
+  //     "eXamine Multiple Language"
+  //   ]
+  // },
+
+];
