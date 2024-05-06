dcea0a78472445f17550f195e30f6ba79f5d48b3
unknown <remyalissonb@gmail.com>
2024-05-06 00:24:25 -0300

    V3

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "No mês de maio o SEST SENAT realiza uma Mobilização Nacional, qual o tema dessa mobilização?",
+    answer: "Maio Amarelo | Segurança no trânsito",
+    options: [
+      "Maio Amarelo | Segurança no trânsito",
+      "Maio Verde | Segurança no meio ambiente",
+      "Maio Branco | Segurança na cidade",
+      "Maio Cinza | conscientização sobre carbono"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Para quem se destina os Serviços do SEST SENAT?",
+    answer: "Para Todos",
+    options: [
+      "Para Todos",
+      "Apenas Vilelenses",
+      "Apenas Transportadores",
+      "Ninguém"
+    ]
+  },
+    {
+    numb: 3,
+    question: "O SEST SENAT tem 3 unidades em Alagoas. Onde estão?",
+    answer: "Arapiraca, Teotônio Vilela e Maceió.",
+    options: [
+      "Maceió, Teotônio Vilela e Delmiro Gouveia.",
+      "Teotônio Vilela, Murici e Arapiraca.",
+      "Arapiraca, Teotônio Vilela e Maceió.",
+      "Murici, Maceió e Teotônio Vilela."
+    ]
+  },
+  //   {
+  //   numb: 4,
+  //   question: "What does SQL stand for?",
+  //   answer: "Structured Query Language",
+  //   options: [
+  //     "Stylish Question Language",
+  //     "Stylesheet Query Language",
+  //     "Statement Question Language",
+  //     "Structured Query Language"
+  //   ]
+  // },
+  //   {
+  //   numb: 5,
+  //   question: "What does XML stand for?",
+  //   answer: "eXtensible Markup Language",
+  //   options: [
+  //     "eXtensible Markup Language",
+  //     "eXecutable Multiple Language",
+  //     "eXTra Multi-Program Language",
+  //     "eXamine Multiple Language"
+  //   ]
+  // },
+
+];
